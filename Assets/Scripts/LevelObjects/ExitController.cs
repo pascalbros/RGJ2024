4a5aab775e5e370b33669a54115b8b7c5a5b6271
takfog <takfog@gmail.com>
2024-09-22 09:33:43 +0200

    wall and level gen prefab

--- Assets/Scripts/LevelObjects/ExitController.cs
+++ Assets/Scripts/LevelObjects/ExitController.cs
@@ -5,6 +5,6 @@ public class ExitController: MonoBehaviour {
     public void OnExit() {
         LevelGenerator.currentLevel += 1;
         string currentSceneName = SceneManager.GetActiveScene().name;
-        SceneManager.LoadScene(currentSceneName);
+        SceneTransitionManager.Instance.ChangeScene(currentSceneName);
     }
 }
